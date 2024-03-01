95b0cb18d5844109894b26c2cb81caa03ffed391
syeda <syeda@gmail.com>
2024-03-01 11:50:18 +0530

    127:create save post feature on  1/03/2024

---
+++ myblog11/src/main/java/com/myblog11/myblog11/service/PostService.java
@@ -0,0 +1,8 @@
+package com.myblog11.myblog11.service;
+
+import com.myblog11.myblog11.payload.PostDto;
+
+public interface PostService {
+    PostDto createPost(PostDto postDto);//method which returns postDto
+
+}
